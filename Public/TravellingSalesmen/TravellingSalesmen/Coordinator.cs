abc7e4b0e44ed0a0311d006e12f92a9d24732f0c
retimarcell55 <jupiter55>
2017-09-25 18:51:47 +0200

    Added public directory (playground :) )

---
+++ Public/TravellingSalesmen/TravellingSalesmen/Coordinator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmen
+{
+    class Coordinator
+    {
+        private Configuration configuration;
+        private Algorithm algorithm;
+        private bool algorithmStarted;
+        private MainForm mainForm;
+
+        public Configuration Configuration { get => configuration; set => configuration = value; }
+        public Algorithm Algorithm { get => algorithm; set => algorithm = value; }
+
+        public Coordinator(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+            algorithmStarted = false;
+        }
+
+        public Coordinator(MainForm mainForm,Configuration configuration,Algorithm algorithm)
+        {
+            this.mainForm = mainForm;
+            this.Configuration = configuration;
+            this.Algorithm = algorithm;
+            algorithmStarted = false;
+        }
+
+        public void startAlgorithm()
+        {
+            algorithm.Initialize();
+
+            mainForm.DrawGraph(configuration.Graph,configuration.AgentManager);
+
+            algorithmStarted = true;
+        }
+
+        public void runAlgorithmNextMove()
+        {
+            if(algorithmStarted)
+            {
+                if(algorithm.hasNonVisitedVertexLeft())
+                {
+                    algorithm.NextTurn();
+                    mainForm.DrawGraph(configuration.Graph, configuration.AgentManager);
+                    mainForm.UpdateResult(algorithm.getActualResult().ToString());
+                }
+                else
+                {
+                    algorithmStarted = false;
+                }
+            }
+        }
+
+        public void runAlgorithmThrough()
+        {
+            if(algorithmStarted)
+            {
+                while (algorithm.hasNonVisitedVertexLeft())
+                {
+                    algorithm.NextTurn();
+                    mainForm.DrawGraph(configuration.Graph, configuration.AgentManager);
+                    mainForm.UpdateResult(algorithm.getActualResult().ToString());
+
+                    System.Threading.Thread.Sleep(500);
+                }
+                algorithmStarted = false;
+            }
+        }
+    }
+}
