1e0da9e9ecc60f0c14c833f308db53149e494de2
Bence <tigerbence@gmail.com>
2017-10-10 02:07:57 +0200

    hamilton/ConsoleApplication1/Program.cs - ben van az egy ügynökös bruteforce hamilton kör algo (GetMinHam fv)

--- Bence/hamilton/ConsoleApplication1/Program.cs
+++ Bence/hamilton/ConsoleApplication1/Program.cs
@@ -8,28 +8,49 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        List<Vertex> Minham = new List<Vertex>();
-        private List<Vertex> permute(List<Vertex> list, int l, int r)
+        public List<Vertex> getMinHam(List<Vertex> list, int l, int r)
+        {
+            List<Vertex> minhamcircle = new List<Vertex>();
+            double minweight = new double();
+            permute(list,l,r,minhamcircle,minweight);
+
+            return minhamcircle;
+        }
+        private void permute(List<Vertex> list, int l, int r, List<Vertex> minhamcircle, double minweight)
         {
             if (l == r)
             {
-                double distance = 0;
-                for (int i = 1; i < list.Count; i++)
+                if(minhamcircle.Count==0)
                 {
-                    distance += Math.Sqrt(Math.Pow(list[i].Position.X - list[i - 1].Position.X, 2) + Math.Pow(list[i].Position.Y - list[i - 1].Position.Y, 2)); 
+                    minhamcircle = list;
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        minweight += Math.Sqrt(Math.Pow(list[i].Position.X - list[i - 1].Position.X, 2) + Math.Pow(list[i].Position.Y - list[i - 1].Position.Y, 2));
+                    }
                 }
-                Minham = list;
+                else
+                {
+                    double d = 0;
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        d += Math.Sqrt(Math.Pow(list[i].Position.X - list[i - 1].Position.X, 2) + Math.Pow(list[i].Position.Y - list[i - 1].Position.Y, 2));
+                    }
+                    if(d<minweight)
+                    {
+                        minhamcircle = list;
+                    }
+                }
+
             }
             else
             {
                 for (int i = l; i <= r; i++)
                 {
                     list = swap(list, l, i);
-                    permute(list, l + 1, r);
+                    permute(list, l + 1, r,minhamcircle, minweight);
                     list = swap(list, l, i);
                 }
-            }
-            return Minham;
+            }  
         }
         public List<Vertex> swap(List<Vertex> a, int i, int j)
         {
@@ -43,9 +64,9 @@ namespace ConsoleApplication1
         }
         static void Main(string[] args)
         {
-           // Program a = new Program();
-           /* Graph g = new Graph();
-            a.permute(g.Vertices,1,g.VertexCount);*/
+           /* Program a = new Program();
+            Graph g = new Graph();
+            List<Vertex> vertices= a.getMinHam(g.Vertices,1,g.Vertices.Count); */
             Console.ReadKey();
         }
     }
